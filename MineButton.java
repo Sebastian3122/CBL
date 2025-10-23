29c405400d5d2d25f3b0ced22fda12e2cd693280
Horia <radulescu.horia.andrei@gmail.com>
2025-10-23 13:25:30 +0300

    Add MineButton.java class

---
+++ MineButton.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MineButton extends JButton {
+    int r; // row
+    int c; // col
+
+    public MineButton(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+}
+
